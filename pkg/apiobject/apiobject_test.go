2a20fd14095024e5fe0a76ac66d3ef830e3ae511
300481 <300481@300481.de>
2020-06-24 17:04:15 +0200

    add test for New()

---
+++ pkg/apiobject/apiobject_test.go
@@ -0,0 +1,40 @@
+package apiobject_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/300481/kitops/pkg/apiobject"
+)
+
+var TestYaml = `
+---
+kind: Test
+metadata:
+  name: Test
+  namespace: Test
+`
+
+var TestObject = apiobject.ApiObject{
+	Kind: "Test",
+	Metadata: struct {
+		Name      string
+		Namespace string
+	}{
+		Name:      "Test",
+		Namespace: "Test",
+	},
+}
+
+func TestNew(t *testing.T) {
+
+	r := bytes.NewReader([]byte(TestYaml))
+	object, err := apiobject.New(r)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if *object != TestObject {
+		t.Errorf("Test Objects don't match.\nTestObject:\n%+v\nNew Object:\n%+v\n", TestObject, *object)
+	}
+}
