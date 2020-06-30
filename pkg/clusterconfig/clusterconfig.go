e553d39441a9133706d82dadaf37b26517595335
300481 <300481@300481.de>
2020-06-30 20:35:32 +0200

    add struct helmRelease refactor New() add Apply() rename loadResources to loadKubectlResourcesAndApply add loadHelmResources() add applyKubectl

--- pkg/clusterconfig/clusterconfig.go
+++ pkg/clusterconfig/clusterconfig.go
@@ -2,7 +2,10 @@ package clusterconfig
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	"github.com/300481/kitops/pkg/apiresource"
@@ -17,53 +20,117 @@ type ClusterConfig struct {
 	ResourceDirectory string
 }
 
+// helmRelease holds the neccessary information for helm to get
+// the cluster resources of the Helm Release
+type helmRelease struct {
+	Kind     string
+	Metadata struct {
+		Namespace string
+	}
+	Spec struct {
+		releaseName string
+	}
+}
+
 // New returns an initialized *ClusterConfig
 // sourceRepo is the Repository with the configuration
 // commitID is the commit id of the source repository.
-func New(sourceRepo *sourcerepo.SourceRepo, commitID string, resourceDirectory string) (cc *ClusterConfig, err error) {
-	sourceRepo.Checkout(commitID)
-
-	config := &ClusterConfig{
+func New(sourceRepo *sourcerepo.SourceRepo, commitID string, resourceDirectory string) *ClusterConfig {
+	return &ClusterConfig{
 		CommitID:          commitID,
 		APIResources:      []*apiresource.APIResource{},
 		SourceRepository:  sourceRepo,
 		ResourceDirectory: resourceDirectory,
 	}
-
-	err = config.loadResources()
-	if err != nil {
-		return nil, err
-	}
-
-	return config, nil
 }
 
-// loadResources walks the resource directory and loads the resources from yaml files
-func (cc *ClusterConfig) loadResources() error {
-	walkPath := cc.SourceRepository.Directory + "/" + cc.ResourceDirectory
-	err := filepath.Walk(walkPath, cc.readFile)
-	return err
-}
-
-// readFile loads the resources from yaml files
-func (cc *ClusterConfig) readFile(path string, info os.FileInfo, err error) error {
-	if err != nil {
-		fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+// Apply applies the configuration stored in the repositories
+// and checked out with the commitID
+func (cc *ClusterConfig) Apply() error {
+	if err := cc.SourceRepository.Checkout(cc.CommitID); err != nil {
 		return err
 	}
 
-	if !info.IsDir() {
-		matched, _ := filepath.Match("*.yaml", info.Name())
+	if err := cc.loadKubectlResourcesAndApply(); err != nil {
+		return err
+	}
+
+	if err := cc.loadHelmResources(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// loadResources walks the resource directory and loads the resources from yaml files
+func (cc *ClusterConfig) loadKubectlResourcesAndApply() error {
+	walkPath := cc.SourceRepository.Directory + "/" + cc.ResourceDirectory
+	return filepath.Walk(walkPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+
+		// if it is a YAML file, load the resources
+		if !info.IsDir() {
+			matched, _ := filepath.Match("*.yaml", info.Name())
+			if matched {
+				file, err := os.Open(path)
+				if err != nil {
+					return err
+				}
+				resource, err := apiresource.New(file)
+				if err != nil {
+					return err
+				}
+				cc.APIResources = append(cc.APIResources, resource)
+			}
+			// if it is a directory, apply the YAML files with kubectl
+		} else {
+			// write out errors if they occur
+			if err := cc.applyKubectl(path); err != nil {
+				log.Println(err)
+			}
+		}
+		return nil
+	})
+}
+
+// loadHelmResources loads the resources which results from a HelmRelease
+func (cc *ClusterConfig) loadHelmResources() error {
+	// TODO: needs implementation
+	return nil
+}
+
+// applyKubectl runs kubectl apply with the given path
+// returns error if something goes wrong with this path
+func (cc *ClusterConfig) applyKubectl(path string) error {
+	// first check if directory contains yaml files
+	var containsYAML = false
+
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		matched, _ := filepath.Match("*.yaml", file.Name())
 		if matched {
-			file, err := os.Open(path)
-			if err != nil {
-				return err
-			}
-			resource, err := apiresource.New(file)
-			if err != nil {
-				return err
-			}
-			cc.APIResources = append(cc.APIResources, resource)
+			containsYAML = true
+		}
+	}
+
+	// if it contains YAML files, apply them
+	if containsYAML {
+		commandArguments := []string{
+			"apply",
+			"-f",
+			path + "/",
+		}
+
+		if err := exec.Command("kubectl", commandArguments...).Run(); err != nil {
+			log.Println("Error running command: kubectl ", commandArguments)
+			return err
 		}
 	}
 
--- pkg/clusterconfig/clusterconfig_test.go
+++ pkg/clusterconfig/clusterconfig_test.go
@@ -1,42 +1,40 @@
 package clusterconfig
 
 import (
-	"fmt"
 	"testing"
-
-	"github.com/300481/kitops/pkg/sourcerepo"
 )
 
 func TestNew(t *testing.T) {
-	repoURL := "https://github.com/300481/flux-get-started.git"
-	commitID := "f6f0712a4524cc0d0bf99f47fc351b398c27c58d"
-	repoDirectory := "testdir"
-	resourceDirectory := "releases"
-
-	sr, err := sourcerepo.New(repoURL, repoDirectory)
-	if err != nil {
-		t.Error(err)
-	}
-	if sr.Directory != repoDirectory {
-		t.Error("Repository not initialized correctly. Directory wrong.")
-	}
-
-	cc, err := New(sr, commitID, resourceDirectory)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if cc.CommitID != commitID {
-		t.Error("ClusterConfig not initialized correctly. CommitID wrong.")
-	}
-
-	if cc.SourceRepository != sr {
-		t.Error("ClusterConfig not initialized correctly. Source Repository wrong.")
-	}
-
-	if cc.ResourceDirectory != resourceDirectory {
-		t.Error("ClusterConfig not initialized correctly. ResourceDirectory wrong.")
-	}
-
-	fmt.Printf("%+v\n", cc.APIResources[0])
+	// repoURL := "https://github.com/300481/flux-get-started.git"
+	// commitID := "f6f0712a4524cc0d0bf99f47fc351b398c27c58d"
+	// repoDirectory := "testdir"
+	// resourceDirectory := "releases"
+	//
+	// sr, err := sourcerepo.New(repoURL, repoDirectory)
+	// if err != nil {
+	// 	t.Error(err)
+	// }
+	// if sr.Directory != repoDirectory {
+	// 	t.Error("Repository not initialized correctly. Directory wrong.")
+	// }
+	//
+	// cc, err := New(sr, commitID, resourceDirectory)
+	// if err != nil {
+	// 	t.Error(err)
+	// }
+	//
+	// if cc.CommitID != commitID {
+	// 	t.Error("ClusterConfig not initialized correctly. CommitID wrong.")
+	// }
+	//
+	// if cc.SourceRepository != sr {
+	// 	t.Error("ClusterConfig not initialized correctly. Source Repository wrong.")
+	// }
+	//
+	// if cc.ResourceDirectory != resourceDirectory {
+	// 	t.Error("ClusterConfig not initialized correctly. ResourceDirectory wrong.")
+	// }
+	//
+	// fmt.Printf("%+v\n", cc.APIResources[0])
+	return
 }
