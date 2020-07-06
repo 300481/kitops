b04d9e5550b06692d105fc8f9fdf632289ca10e7
300481 <300481@300481.de>
2020-07-06 17:54:50 +0200

    add repo to kitops instance

--- pkg/kitops/kitops.go
+++ pkg/kitops/kitops.go
@@ -3,23 +3,36 @@ package kitops
 import (
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/300481/kitops/pkg/sourcerepo"
 	"github.com/gorilla/mux"
 )
 
 // Kitops is the instance type
 type Kitops struct {
-	router *mux.Router
+	router     *mux.Router
+	repository *sourcerepo.SourceRepo
 }
 
 // New returns a new Kitops instance
 func New() *Kitops {
+	url := os.Getenv("KITOPS_DEPLOYMENTS_URL")
+	repo, err := sourcerepo.New(url, "/tmp/repo")
+
+	if err != nil {
+		log.Printf("unable to get repository: %s\n%v", url, err)
+		return nil
+	}
+
 	return &Kitops{
-		router: mux.NewRouter(),
+		router:     mux.NewRouter(),
+		repository: repo,
 	}
 }
 
 // Serve runs the application in server mode
 func (k *Kitops) Serve() {
+	k.routes()
 	log.Fatal(http.ListenAndServe(":8080", k.router))
 }
