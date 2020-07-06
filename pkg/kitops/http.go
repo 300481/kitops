dc55cf9946a5334ec3dc02b0d96627e6bcdcf183
300481 <300481@300481.de>
2020-07-06 22:01:04 +0200

    add apply handler

--- pkg/kitops/http.go
+++ pkg/kitops/http.go
@@ -1,6 +1,7 @@
 package kitops
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -9,10 +10,30 @@ import (
 // routes sets the routes
 func (k *Kitops) routes() {
 	k.router.HandleFunc("/healthz", k.healthHandler)
+	k.router.HandleFunc("/apply", k.applyHandler)
 }
 
 // healthHandler handles the /healthz endpoint
 func (k *Kitops) healthHandler(w http.ResponseWriter, r *http.Request) {
+	// respond OK
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "OK")
+}
+
+// applyHandler handles the /apply endpoint
+func (k *Kitops) applyHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("apply.handler:", r.Method, "request from ", r.RemoteAddr)
+
+	commitID := r.URL.Query().Get("commitid")
+
+	if len(commitID) < 1 {
+		log.Println("apply.handler got no commitID")
+		handleError(fmt.Errorf("apply.handler got no commitID"), w)
+		return
+	}
+
+	log.Printf("apply.handler got commitID: %s\n", commitID)
+
 	// respond OK
 	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, "OK")
