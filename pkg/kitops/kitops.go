40a1166cb962a991600f78cd3145fb395d2360f6
300481 <300481@300481.de>
2020-07-07 17:19:23 +0200

    add queue processing

--- pkg/kitops/kitops.go
+++ pkg/kitops/kitops.go
@@ -5,20 +5,23 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/300481/kitops/pkg/clusterconfig"
+	"github.com/300481/kitops/pkg/queue"
 	"github.com/300481/kitops/pkg/sourcerepo"
 	"github.com/gorilla/mux"
 )
 
 // Kitops is the instance type
 type Kitops struct {
-	router     *mux.Router
-	repository *sourcerepo.SourceRepo
+	router *mux.Router
+	queue  *queue.Queue
 }
 
 // New returns a new Kitops instance
 func New() *Kitops {
 	url := os.Getenv("KITOPS_DEPLOYMENTS_URL")
 	if len(url) == 0 {
+		// set default URL
 		url = "https://github.com/300481/kitops-test.git"
 	}
 
@@ -29,9 +32,14 @@ func New() *Kitops {
 		return nil
 	}
 
+	q := queue.New(&queueProcessor{
+		clusterConfigs: make(map[string]*clusterconfig.ClusterConfig),
+		repository:     repo,
+	})
+
 	return &Kitops{
-		router:     mux.NewRouter(),
-		repository: repo,
+		router: mux.NewRouter(),
+		queue:  q,
 	}
 }
 
@@ -39,4 +47,25 @@ func New() *Kitops {
 func (k *Kitops) Serve() {
 	k.routes()
 	log.Fatal(http.ListenAndServe(":8080", k.router))
+}
+
+// QueueProcessor is the instance for processsing the queue items
+type queueProcessor struct {
+	clusterConfigs map[string]*clusterconfig.ClusterConfig
+	repository     *sourcerepo.SourceRepo
+}
+
+// Process processes new queued items
+func (qp *queueProcessor) Process(q *queue.Queue) {
+	commitID := q.StartNext().(string)
+
+	qp.clusterConfigs[commitID] = clusterconfig.New(qp.repository, commitID, "/tmp/repo")
+
+	if err := qp.clusterConfigs[commitID].Apply(); err != nil {
+		q.Finish(false)
+	} else {
+		q.Finish(true)
+	}
+
+	return
 }
