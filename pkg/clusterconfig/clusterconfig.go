8723abe13199b2c72f26448e05e303f59ef81f8f
300481 <300481@300481.de>
2020-07-10 16:14:01 +0200

    reimplement ClusterConfig

--- pkg/clusterconfig/clusterconfig.go
+++ pkg/clusterconfig/clusterconfig.go
@@ -1,8 +1,6 @@
 package clusterconfig
 
 import (
-	"bytes"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -15,152 +13,77 @@ import (
 
 // ClusterConfig holds all API Resources for a commit id
 type ClusterConfig struct {
-	CommitID          string
-	APIResources      []*apiresource.APIResource
-	SourceRepository  *sourcerepo.SourceRepo
-	ResourceDirectory string
-	//HelmReleases      []*helmrelease.HelmRelease
+	APIResources     *apiresource.Collection
+	SourceRepository *sourcerepo.SourceRepo
+	CommitID         string
 }
 
 // New returns an initialized *ClusterConfig
 // sourceRepo is the Repository with the configuration
 // commitID is the commit id of the source repository.
 func New(sourceRepo *sourcerepo.SourceRepo, commitID string, resourceDirectory string) *ClusterConfig {
-	log.Println("Create new ClusterConfig.")
 	return &ClusterConfig{
-		CommitID:          commitID,
-		APIResources:      []*apiresource.APIResource{},
-		SourceRepository:  sourceRepo,
-		ResourceDirectory: resourceDirectory,
-		//HelmReleases:      []*helmrelease.HelmRelease{},
+		APIResources:     apiresource.NewCollection(),
+		SourceRepository: sourceRepo,
+		CommitID:         commitID,
 	}
 }
 
-// Apply applies the configuration stored in the repositories
+// ApplyManifests applies the manifests stored in the repository
 // and checked out with the commitID.
-// It also loads all Resources into the ClusterConfig.
-func (cc *ClusterConfig) Apply() error {
+// It returns an error if something goes wrong on apply.
+func (cc *ClusterConfig) ApplyManifests() error {
 	log.Println("Apply the ClusterConfig.")
+
 	if err := cc.SourceRepository.Checkout(cc.CommitID); err != nil {
 		log.Printf("Checkout of repository failed. Commit: %s", cc.CommitID)
 		return err
 	}
-	walkPath := cc.SourceRepository.Directory + "/" + cc.ResourceDirectory
-	walkErr := filepath.Walk(walkPath, func(path string, info os.FileInfo, err error) error {
+
+	walkPath := cc.SourceRepository.Directory
+	return filepath.Walk(walkPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			log.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
 			return err
 		}
 
-		// if it is a YAML file, load the resources
-		if !info.IsDir() {
-			matched, _ := filepath.Match("*.yaml", info.Name())
-			if matched {
-				content, err := ioutil.ReadFile(path)
-				if err != nil {
-					return err
-				}
-
-				// Create a Reader for the API Resources
-				APIResourceContentReader := bytes.NewReader(content)
-				for {
-					resource, err := apiresource.New(APIResourceContentReader)
-					if err != nil {
-						break
-					}
-					cc.APIResources = append(cc.APIResources, resource)
-				}
-
-				// // Create a Reader for the HelmReleases
-				// HelmReleaseContentReader := bytes.NewReader(content)
-				// for {
-				// 	helmRelease, err := helmrelease.New(HelmReleaseContentReader)
-				// 	if err != nil {
-				// 		break
-				// 	}
-				// 	if helmRelease != nil {
-				// 		cc.HelmReleases = append(cc.HelmReleases, helmRelease)
-				// 	}
-				// }
+		// if it is a directory, apply the manifests
+		if info.IsDir() {
+			files, err := ioutil.ReadDir(path)
+			if err != nil {
+				log.Printf("error reading directory: %q: %v\n", path, err)
+				return nil
 			}
-			// if it is a directory, apply the YAML files with kubectl
-		} else {
-			// write out errors if they occur
-			if err := cc.applyKubectl(path); err != nil {
-				log.Println(err)
-				return err
+
+			// check if it contains YAML files
+			var containsYAML = false
+			for _, file := range files {
+				matched, _ := filepath.Match("*.yaml", file.Name())
+				containsYAML = containsYAML || matched
+				if matched {
+					break
+				}
+			}
+
+			if containsYAML {
+				commandArguments := []string{
+					"apply",
+					"-f",
+					path + "/",
+				}
+
+				if err := exec.Command("kubectl", commandArguments...).Run(); err != nil {
+					log.Println("Error running command: kubectl ", commandArguments)
+					return nil
+				}
 			}
 		}
-		// return cc.loadHelmResources()
 		return nil
 	})
-	return walkErr
 }
 
-// applyKubectl runs kubectl apply with the given path
-// returns error if something goes wrong with this path
-func (cc *ClusterConfig) applyKubectl(path string) error {
-	// first check if directory contains yaml files
-	var containsYAML = false
-
-	files, err := ioutil.ReadDir(path)
-	if err != nil {
-		return err
-	}
-
-	for _, file := range files {
-		matched, _ := filepath.Match("*.yaml", file.Name())
-		if matched {
-			containsYAML = true
-		}
-	}
-
-	// if it contains YAML files, apply them
-	if containsYAML {
-		commandArguments := []string{
-			"apply",
-			"-f",
-			path + "/",
-		}
-
-		if err := exec.Command("kubectl", commandArguments...).Run(); err != nil {
-			log.Println("Error running command: kubectl ", commandArguments)
-			return err
-		}
-	}
-
-	return nil
+// LoadManifests loads the manifests of the checked out repository
+// into the ClusterConfig
+func (cc *ClusterConfig) LoadManifests() error {
+	return cc.APIResources.LoadFromDirectory(cc.SourceRepository.Directory)
 }
-
-// // loadHelmResources loads the resources which results from a HelmRelease
-// func (cc *ClusterConfig) loadHelmResources() error {
-// 	var command string
-// 	commandArguments := []string{
-// 		"get",
-// 		"manifest",
-// 	}
-//
-// 	for _, helmrelease := range cc.HelmReleases {
-// 		if helmrelease.Spec.HelmVersion == "v3" {
-// 			command = "helm3"
-// 			commandArguments = append(commandArguments, "--namespace", helmrelease.Metadata.Namespace)
-// 		} else {
-// 			command = "helm2"
-// 		}
-//
-// 		b, err := exec.Command(command, commandArguments...).Output()
-// 		if err != nil {
-// 			return err
-// 		}
-//
-// 		APIResourceContentReader := bytes.NewReader(b)
-// 		for {
-// 			resource, err := apiresource.New(APIResourceContentReader)
-// 			if err != nil {
-// 				break
-// 			}
-// 			cc.APIResources = append(cc.APIResources, resource)
-// 		}
-// 	}
-// 	return nil
-// }
--- pkg/clusterconfig/go.mod
+++ pkg/clusterconfig/go.mod
@@ -3,7 +3,6 @@ module github.com/300481/kitops/pkg/clusterconfig
 go 1.14
 
 require (
-	github.com/300481/kitops/pkg/apiresource v0.0.0-20200701210506-414723a79c43
-	github.com/300481/kitops/pkg/helmrelease v0.0.0-20200701210506-414723a79c43
-	github.com/300481/kitops/pkg/sourcerepo v0.0.0-20200701210506-414723a79c43
+	github.com/300481/kitops/pkg/apiresource v0.0.0-20200710132642-282acb03db58
+	github.com/300481/kitops/pkg/sourcerepo v0.0.0-20200708212255-b69e867e11ce
 )
