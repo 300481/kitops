d321d78df24ffc686dcfc24b8ea9e102b24a9513
300481 <300481@300481.de>
2020-07-25 22:58:15 +0200

    split queueprocessor in an own file

--- pkg/kitops/kitops.go
+++ pkg/kitops/kitops.go
@@ -52,34 +52,3 @@ func (k *Kitops) Serve() {
 	k.routes()
 	log.Fatal(http.ListenAndServe(":8080", k.router))
 }
-
-// QueueProcessor is the instance for processsing the queue items
-type QueueProcessor struct {
-	ClusterConfigs map[string]*clusterconfig.ClusterConfig
-	repository     *sourcerepo.SourceRepo
-}
-
-// Process processes new queued commitIDs
-func (qp *QueueProcessor) Process(q *queue.Queue) {
-	commitID := q.StartNext().(string)
-
-	// create a new ClusterConfig
-	cc := clusterconfig.New(qp.repository, commitID)
-	qp.ClusterConfigs[commitID] = cc
-
-	// apply the manifests
-	if err := cc.ApplyManifests(); err != nil {
-		log.Printf("failed to apply manifests of commitID: %s", commitID)
-		q.Finish(false)
-	} else {
-		q.Finish(true)
-	}
-
-	// load the manifests in the ClusterConfig
-	if err := cc.LoadManifests(); err != nil {
-		log.Printf("failed to load manifests of commitID: %s", commitID)
-	}
-
-	// label the api resources
-	qp.ClusterConfigs[commitID].Label()
-}
