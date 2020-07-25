248fc402829ef4226c590c3cf97b256526cfd998
300481 <300481@300481.de>
2020-07-25 22:46:30 +0200

    split namespaced to an own file

--- pkg/apiresource/apiresource.go
+++ pkg/apiresource/apiresource.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"log"
 	"os/exec"
-	"strings"
 
 	"gopkg.in/yaml.v3"
 )
@@ -110,50 +109,4 @@ func (r *APIResource) Checksum() string {
 	s := fmt.Sprintf("%v", *r)
 	sum := sha256.Sum256([]byte(s))
 	return fmt.Sprintf("%x", sum)
-}
-
-// namespaced struct holds the dynamic information if Kind is Namespaced
-type namespaced struct {
-	resource map[string]bool
-}
-
-// declare package variable
-var ns *namespaced
-
-func (n *namespaced) namespaced(kind string) bool {
-	n.update()
-	return n.resource[kind]
-}
-
-func (n *namespaced) set(kind string, namespaced bool) {
-	n.resource[kind] = namespaced
-}
-
-func (n *namespaced) update() {
-	output, err := exec.Command("kubectl", "api-resources").Output()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	lines := strings.Split(string(output), "\n")
-	for _, line := range lines[1:] {
-		if len(line) == 0 {
-			break
-		}
-
-		s := strings.Fields(line)
-		kind := s[len(s)-1]
-		namespaced := s[len(s)-2] == "true"
-
-		ns.set(kind, namespaced)
-	}
-}
-
-// initialize namespaced
-func init() {
-	ns = &namespaced{
-		resource: make(map[string]bool),
-	}
-	ns.update()
 }
---
+++ pkg/apiresource/namespaced.go
@@ -0,0 +1,53 @@
+package apiresource
+
+// namespaced struct holds the dynamic information if Kind is Namespaced
+import (
+	"log"
+	"os/exec"
+	"strings"
+)
+
+type namespaced struct {
+	resource map[string]bool
+}
+
+// declare package variable
+var ns *namespaced
+
+func (n *namespaced) namespaced(kind string) bool {
+	n.update()
+	return n.resource[kind]
+}
+
+func (n *namespaced) set(kind string, namespaced bool) {
+	n.resource[kind] = namespaced
+}
+
+func (n *namespaced) update() {
+	output, err := exec.Command("kubectl", "api-resources").Output()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	lines := strings.Split(string(output), "\n")
+	for _, line := range lines[1:] {
+		if len(line) == 0 {
+			break
+		}
+
+		s := strings.Fields(line)
+		kind := s[len(s)-1]
+		namespaced := s[len(s)-2] == "true"
+
+		ns.set(kind, namespaced)
+	}
+}
+
+// initialize namespaced
+func init() {
+	ns = &namespaced{
+		resource: make(map[string]bool),
+	}
+	ns.update()
+}
