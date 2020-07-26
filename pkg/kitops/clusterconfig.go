fb6e5442aa0e46d1a6b21eb92cd0975791ffa2c5
300481 <300481@300481.de>
2020-07-26 21:33:13 +0200

    extend logging for clusterconfig.Clean()

--- pkg/kitops/clusterconfig.go
+++ pkg/kitops/clusterconfig.go
@@ -150,7 +150,7 @@ func (cc *ClusterConfig) Clean() {
 	// compare them with the resources of the current ClusterConfig
 	// if not in the current ClusterConfig, delete it
 	for hash, item := range tempCollection.Items {
-		log.Printf("Cleanup check for Checksum: %s", hash)
+		log.Printf("Cleanup check for Checksum: %s %s %s %s", hash, item.Kind, item.Metadata.Name, item.Metadata.Namespace)
 		_, ok := cc.APIResources.Items[hash]
 		if !ok {
 			item.Delete()
